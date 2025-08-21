560abf679a6204b08133e85f9d11e296a608f0c8
Dhruba <dhrubajyoti1212@gmail.com>
2025-08-21 12:54:43 +0600

    studentcontroller updated

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ResourceController;
+//use App\Http\Controllers\ResourceController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -36,5 +37,6 @@ Route::prefix('greeting')->group(function () {
 Route::view('/master','master');
 Route::view('/feature1','feature1');
 Route::view('/feature2','feature2');
+Route::get('student',[StudentController::class, 'index']);
 
-Route::resource('student', ResourceController::class);
+//Route::resource('student', ResourceController::class);
